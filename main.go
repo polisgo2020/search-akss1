0befa869f831f970dc91dc1efe5b5fbe07ef078d
Adil Akkulov <adil-akkulov@mail.ru>
2020-04-06 00:28:02 +0300

    Add http server with "search" command
    Update readme

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"searchera/indexio"
+	"searchera/servhandlers"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/urfave/cli/v2"
 )
 
@@ -21,6 +26,22 @@ func main() {
 	}
 
 	app.Commands = []*cli.Command{
+		{
+			Name:  "server",
+			Usage: "Read index and starts the http server on the specified host and port",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:        "host",
+					DefaultText: "127.0.0.1",
+				},
+				&cli.StringFlag{
+					Name:        "port",
+					DefaultText: "8080",
+				},
+				idxFlag,
+			},
+			Action: ProcessServer,
+		},
 		{
 			Name:    "make",
 			Aliases: []string{"m"},
@@ -55,6 +76,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func ProcessServer(c *cli.Context) error {
+	host := c.String("host")
+	port := c.String("port")
+	idxPath := c.String("index")
+
+	addr := net.JoinHostPort(host, port)
+
+	idx, err := indexio.ReadIndex(idxPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Index '%s' read succesfull", idxPath)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		servhandlers.SearchHandler(w, r, idx, idxPath)
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    addr,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+
+	return nil
 }
 
 func ProcessMakeIdx(c *cli.Context) error {
